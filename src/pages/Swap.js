e8cfa798cf7af24bda1b48771752bb5b05342bcb
rapiddeveloper0217 <rapiddevelop0217@gmail.com>
2022-08-17 19:35:18 +0900

    8/17 frontend

--- src/pages/Swap.js
+++ src/pages/Swap.js
@@ -2,56 +2,24 @@ import { setup1inchWidget } from "@1inch/embedded-widget";
 import { useEffect, useState } from "react";
 import $ from "jquery";
 import "../assets/css/Swap.css";
-
 function Swap() {
-<<<<<<< HEAD
-    const [loaded, setLoad] = useState(false);
-    useEffect(() => {
-        const swapAggregator = setup1inchWidget({
-            chainId: 137,
-            sourceTokenSymbol: '1INCH',
-            destinationTokenSymbol: 'DAI',
-            hostElement: document.getElementById("v-aggregator"),
-            // provider: window.ethereum,
-            theme: 'dark',
-            sourceTokenAmount: '15'
-        });
-        console.log(swapAggregator);
-        swapAggregator.onIframeLoad(() => {
-            setLoad(true);
-            console.log("############");
-
-
-        })
-    }, [])
-    return (
-        <>
-
-            <div id="v-aggregator" className='w-full h-screen border-solid' style={loaded ? { "visibility": "visible" } : { "visibility": "hidden" }} >
-            </div>
-
-        </>
-    )
-
-=======
   const [loaded, setLoad] = useState(false);
   useEffect(() => {
     const swapAggregator = setup1inchWidget({
       chainId: 137,
-      sourceTokenSymbol: "1INCH",
-      destinationTokenSymbol: "DAI",
+      sourceTokenSymbol: '1INCH',
+      destinationTokenSymbol: 'DAI',
       hostElement: document.getElementById("v-aggregator"),
       // provider: window.ethereum,
-      theme: "dark",
-      sourceTokenAmount: "15",
+      theme: 'dark',
+      sourceTokenAmount: '15'
     });
     console.log(swapAggregator);
     swapAggregator.onIframeLoad(() => {
       setLoad(true);
-      console.log("############");
-      // var k = $("#oneInchWidgetFrame").contents().hide();
-    });
-  }, []);
+
+    })
+  }, [])
   return (
     <>
       <br />
@@ -63,7 +31,7 @@ function Swap() {
       ></div>
     </>
   );
->>>>>>> 73235f09c4d9f6c28e6c27209907d391bcbaabab
+
 }
 
 export default Swap;
